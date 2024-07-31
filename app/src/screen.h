c9a98bb2a54460783f6ba96809d9220cdafa2fca
Trung Hieu Le <trunghieu.le@nxp.com>
2024-07-31 11:54:47 +0200

    scrmirror: Change to RGB565 to save bandwidth
    Change from XRGB32 to RGB565 to save bandwidth
    
    Signed-off-by: Trung Hieu Le <trunghieu.le@nxp.com>

--- app/src/screen.h
+++ app/src/screen.h
@@ -3,7 +3,7 @@
 
 #define SCR_WIDTH  DT_PROP(DT_CHOSEN(zephyr_display), width)
 #define SCR_HEIGHT DT_PROP(DT_CHOSEN(zephyr_display), height)
-#define SCR_BUF_SZ (SCR_WIDTH * SCR_HEIGHT * 4)
-#define SCR_FORMAT PIXEL_FORMAT_ARGB_8888
+#define SCR_BUF_SZ (SCR_WIDTH * SCR_HEIGHT * 2)
+#define SCR_FORMAT PIXEL_FORMAT_RGB_565
 
 #endif
